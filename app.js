7c25fec7749882c1bcbb5591820dd91931f6639e
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2024-03-26 20:30:12 -0500

    added random items

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var instrumentsRouter = require('./routes/instruments');
+var gridRouter = require('./routes/grid');
+var randomRouter = require('./routes/pick');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/instruments',instrumentsRouter);
+app.use('/grid',gridRouter);
+app.use('/randomitem',randomRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
